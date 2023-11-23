e8a0eaed4bc3f22a9178d55194ef1898498eaae9
Jackelam20 <jackelam20@gmail.com>
2023-11-22 20:08:54 -0600

    apiandroutes

---
+++ server/controllers/chatroom.js
@@ -0,0 +1,42 @@
+import chatRoom from "../models/Groups.js";
+
+export const createChatRoom = async (req, res) => {
+  const newChatRoom = new chatRoom({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+
+  try {
+    await newChatRoom.save();
+    res.status(201).json(newChatRoom);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
+
+export const getChatRoomOfUser = async (req, res) => {
+  try {
+    const chatRoom = await chatRoom.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(chatRoom);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+export const getChatRoomOfUsers = async (req, res) => {
+  try {
+    const chatRoom = await ChatRoom.find({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    res.status(200).json(chatRoom);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
